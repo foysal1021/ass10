c40809134ac1da97bcae60f0f92986e228168a86
foysal1021 <foysala1021@gmail.com>
2022-10-27 21:52:31 +0600

    added footer

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-    return (
-        <div>
-            <h1> this is footer </h1>
-        </div>
-    );
+  return (
+    <div className=" container-fluid bg-primary">
+      <div className="  p-5 text-light container">
+        <h3> Grow your career</h3>
+        <h4> Start Learning With FOYSAL AHAMED Academy</h4>
+        <Button variant="light">
+          {" "}
+          <Link
+            className=" text-primary fs-5  text-decoration-none text-uppercase"
+            to="/"
+          >
+            {" "}
+            Our Course{" "}
+          </Link>{" "}
+        </Button>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
